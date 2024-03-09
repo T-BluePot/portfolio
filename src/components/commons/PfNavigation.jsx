6be455716dffd97ee25e301bf959f2950d88b9a8
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-03-10 02:19:52 +0900

    feat navigation bar done

--- src/components/commons/PfNavigation.jsx
+++ src/components/commons/PfNavigation.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import '@/styles/commons/PfNavigation.css'
 import IconHamburger from './IconHamburger'
 import PfButton from './PfButton'
-// import { ReactComponent as PfBluePotLogo } from '@/assets/pf-blue-pot-logo.svg'
+import PfBluePotLogo from '@/assets/pf-blue-pot-logo.svg?react'
 
 const PfNavigation = () => {
   return (
     <div className="w-full h-14 pf-navigation flex">
-      <div className="w-full h-full flex flex-1 items-center bg-red-500">
+      <div className="w-full h-full flex flex-1 items-center">
         <IconHamburger className=""></IconHamburger>
       </div>
-      <div className="w-full h-full flex flex-[2_2_0%] bg-blue-500">
-        {/* <PfBluePotLogo width="24" height="30" fill="white"/> */}
+      <div className="w-full h-full flex flex-[2_2_0%] justify-center items-center">
+        <PfBluePotLogo
+          width="24"
+          height="30"
+          fill="white"
+        />
       </div>
-      <div className="w-full min-w-36 h-full flex flex-1 justify-end  bg-red-500">
+      <div className="w-full min-w-36 h-full flex flex-1 justify-end ">
         <PfButton buttonText="Contact us" />
       </div>
     </div>
