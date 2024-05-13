92c339044718419c1215337509a624bdaa9f022e
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-05-14 01:06:03 +0900

    add slide button opacity

--- src/components/commons/PSlidingButton.jsx
+++ src/components/commons/PSlidingButton.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import {React, useEffect} from 'react'
+import { motion } from 'framer-motion'
 import '@/styles/commons/PSlidingButton.css'
 import PScrollArrow from '@/assets/arrow.svg?react'
 
 const PSlidingButton = () => {
+  // const { scrollY, scrollYProgress } = useScroll();
+  useEffect(()=>{})
   return (
-    <div className="flex justify-center">
+    <motion.div
+      initial={{
+        opacity: 0
+      }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={false}
+      transition={{
+        // animate state까지 어떻게 변할지 정하는 옵션
+        // 여러 transition type을 정의 할 수 있다.
+        ease: 'easeInOut',
+        duration: 0.5
+      }}
+      className="flex justify-center">
       <button id="arrow">
         <PScrollArrow />
       </button>
-    </div>
+    </motion.div>
   )
 }
 
