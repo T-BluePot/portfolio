ac0413cb1940bf34e46154ff7e31c372c75853b6
Leeseoleem <leeseorim0029@gmail.com>
2025-05-09 18:05:18 +0900

    feat: 팀 소개 페이지 구현 완료

--- src/pages/layout/team-intro/TeamIntro.tsx
+++ src/pages/layout/team-intro/TeamIntro.tsx
@@ -1,21 +1,67 @@
 import styles from './TeamIntro.module.scss';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { Colors } from '@/styles/colors';
 
 export default function TeamIntro() {
-  const { scrollY } = useScroll();
   return (
     <section id="team-intro" className={styles.TeamIntro}>
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 0,
-          color: Colors.gray3,
-        }}
-      >
-        푸른핫가마,
-      </motion.h1>
+      <div className={styles.content}>
+        <div>
+          <motion.h1
+            className={styles.title}
+            initial={{
+              opacity: 0,
+              y: 40,
+              color: Colors.gray3,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              color: Colors.pink1,
+            }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            푸른핫가마,
+          </motion.h1>
+          <motion.h1
+            className={styles.title}
+            initial={{
+              opacity: 0,
+              y: 40,
+              color: Colors.gray3,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              color: Colors.blue1,
+            }}
+            transition={{ duration: 1, delay: 0.5 }}
+            viewport={{ once: true }}
+          >
+            함께 만든 불꽃
+          </motion.h1>
+        </div>
+        <motion.p
+          className={styles.subTitle}
+          initial={{
+            opacity: 0,
+            y: 40,
+            color: Colors.gray3,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+            color: Colors.black,
+          }}
+          transition={{ duration: 1, delay: 1 }}
+          viewport={{ once: true }}
+        >
+          소박하지만 뜨거운 열정이 모여, <br />
+          다양한 도전 속에서 함께 성장해온 우리의 이야기입니다.
+        </motion.p>
+      </div>
     </section>
   );
 }
