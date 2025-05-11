8f0fb3196d29c505be3e3e974f1b25a44389ee76
Leeseoleem <leeseorim0029@gmail.com>
2025-05-11 22:26:28 +0900

    feat: Wavw Two 컴포넌트 추가

--- src/pages/layout/transition/WaveTransition.tsx
+++ src/pages/layout/transition/WaveTransition.tsx
@@ -1,10 +1,12 @@
 import styles from './WaveTransition.module.scss';
 import WaveOne from './components/WaveOne';
+import WaveTwo from './components/WaveTwo';
 
 export default function WaveTransition() {
   return (
     <section className={styles.wave}>
       <WaveOne />
+      <WaveTwo />
     </section>
   );
 }
--- src/pages/layout/transition/components/WaveTwo.tsx
+++ src/pages/layout/transition/components/WaveTwo.tsx
@@ -2,19 +2,19 @@ import { motion } from 'framer-motion';
 import Wave from 'react-wavify';
 import { Colors } from '@/styles/colors';
 
-import styles from './WaveOne.module.scss';
+import styles from './WaveTwo.module.scss';
 
-export default function WaveOne() {
+export default function WaveTwo() {
   return (
     <motion.div
       className={styles.waveFramer}
       initial={{ y: 300 }} // 시작 상태: 흐리고 아래에 있음
       whileInView={{ y: 1 }} // 뷰포트에 들어오면: 선명 + 제자리
-      transition={{ duration: 1.5, ease: 'easeOut' }} // 부드럽게 올라옴
+      transition={{ duration: 2, ease: 'easeOut' }} // 부드럽게 올라옴
       viewport={{ once: true }} // 한 번만 실행
     >
       <Wave
-        fill={Colors.pink2}
+        fill={Colors.pink3}
         paused={false}
         options={{
           height: 60,
