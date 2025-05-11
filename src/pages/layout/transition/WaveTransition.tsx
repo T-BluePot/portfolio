2e4abe68988782765af89d44c33b4d48c7d8593a
Leeseoleem <leeseorim0029@gmail.com>
2025-05-11 20:53:00 +0900

    feat: Wave one 컴포넌트 생성

--- src/pages/layout/transition/WaveTransition.tsx
+++ src/pages/layout/transition/WaveTransition.tsx
@@ -1,10 +1,10 @@
 import styles from './WaveTransition.module.scss';
-import Wave from 'react-wavify';
+import WaveOne from './components/WaveOne';
 
 export default function WaveTransition() {
   return (
     <section className={styles.wave}>
-      <p>wave section</p>
+      <WaveOne />
     </section>
   );
 }
---
+++ src/pages/layout/transition/components/WaveOne.tsx
@@ -0,0 +1,29 @@
+import { motion } from 'framer-motion';
+import Wave from 'react-wavify';
+import { Colors } from '@/styles/colors';
+
+import styles from './WaveOne.module.scss';
+
+export default function WaveOne() {
+  return (
+    <motion.div
+      className={styles.waveFramer}
+      initial={{ opacity: 0.8, y: 240 }} // 시작 상태: 흐리고 아래에 있음
+      whileInView={{ opacity: 1, y: 1 }} // 뷰포트에 들어오면: 선명 + 제자리
+      transition={{ duration: 1.5, ease: 'easeOut' }} // 부드럽게 올라옴
+      viewport={{ once: true }} // 한 번만 실행
+    >
+      <Wave
+        fill={Colors.pink2}
+        paused={false}
+        options={{
+          height: 60,
+          amplitude: 30,
+          speed: 0.5,
+          points: 2,
+        }}
+        className={styles.wave}
+      />
+    </motion.div>
+  );
+}
