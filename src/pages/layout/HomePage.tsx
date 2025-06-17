0eaa4abffc5f2b032b8a4dca034c859539e53a97
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-06-17 14:37:47 +0900

    [FEAT] transition wave  섹션 추가 (#34)
    * chore: WaveTransition 컴포넌트 파일 추가
    
    * chore: React Wavify 라이브러리 설치
    
    * chore: homepage에 wave 섹션 추가
    
    * design: wave 섹션 크기 설정
    
    - 기본 섹션과 동일한 크기로 지정하였으나, 효과 지정 후 어색하면 늘릴 계획
    
    * chore: wave 컴포넌트에 framer motion 추가
    
    * feat: Wave one 컴포넌트 생성
    
    * css: 웨이브 각도 조절
    
    * feat: Wavw Two 컴포넌트 추가

--- src/pages/layout/HomePage.tsx
+++ src/pages/layout/HomePage.tsx
@@ -1,4 +1,5 @@
 import MainHero from './main-hero/MainHero';
+import WaveTransition from './transition/WaveTransition';
 import TeamIntro from './team-intro/TeamIntro';
 import AboutUs from './about-us/AboutUs';
 
@@ -6,6 +7,7 @@ export default function HomePage() {
   return (
     <>
       <MainHero />
+      <WaveTransition />
       <TeamIntro />
       <AboutUs />
       {/* 필요한 섹션 계속 추가 가능 */}
